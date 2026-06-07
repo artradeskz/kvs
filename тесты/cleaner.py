14bd2584c5e10d3070984caefc9f85cee72cb0aa
mzhoot <artrades_kz@mail.ru>
2026-06-07 15:50:48 +0300

    починить бнд и учесть длину коментария

--- тесты/cleaner.py
+++ тесты/cleaner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-extensions = {'.аст', '.csv', '.токены', '.проход1', '.elf', '.log'}
+extensions = {'.аст', '.csv', '.токены', '.проход1', '.elf', '.log', '.txt'}
 
 for file in os.listdir('.'):
     if os.path.isfile(file) and os.path.splitext(file)[1] in extensions:
